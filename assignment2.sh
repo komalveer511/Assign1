48cc6a8870f8751be57d8275aa9735d3b15a6411
komalveer511 <133136651+komalveer511@users.noreply.github.com>
2024-11-13 21:45:58 -0500

    assignment2.sh

--- assignment2.sh
+++ assignment2.sh
@@ -1,89 +1,85 @@
 #!/bin/bash
 
-# Set hostname
-hostname="autosrv"
-sudo hostnamectl set-hostname "$hostname"
-echo **SUCCESS**
+# Set variables for hostname and network configurations
+HOSTNAME="autosrv"
+STATIC_IP="192.168.16.21/24"
+GATEWAY="192.168.16.1"
+DNS="192.168.16.1"
+SEARCH_DOMAINS="home.arpa localdomain"
+USERS=("dennis" "aubrey" "captain" "snibbles" "brownie" "scooter" "sandy" "perrier" "cindy" "tiger" "yoda")
 
-# Set static IP address
-sudo bash -c 'echo "auto lo
+# Set the hostname
+echo "Setting hostname to $HOSTNAME"
+sudo hostnamectl set-hostname "$HOSTNAME"
+echo "**Hostname configured successfully**"
+
+# Configure static IP address
+echo "Configuring static IP address: $STATIC_IP"
+sudo bash -c "cat <<EOF > /etc/network/interfaces
+auto lo
 iface lo inet loopback
 
 auto eth0
 iface eth0 inet static
-    address 192.168.16.21/24
-    gateway 192.168.16.1
-    dns-nameservers 192.168.16.1
-    dns-search home.arpa localdomain" > /etc/network/interfaces'
-echo **SUCCESS**
+    address $STATIC_IP
+    gateway $GATEWAY
+    dns-nameservers $DNS
+    dns-search $SEARCH_DOMAINS
+EOF"
+echo "**Network configuration set successfully**"
 
-# Install required software
+# Update system and install required software
+echo "Updating package list and installing required software"
 sudo apt update
 sudo apt-get install -y openssh-server apache2 squid ufw
-echo **SUCCESS**
+echo "**Software installation completed successfully**"
 
-# Configure SSH server
+# Configure SSH server to disable password authentication
+echo "Configuring SSH to disable password authentication"
 sudo sed -i '/PasswordAuthentication/d' /etc/ssh/sshd_config
-echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
-systemctl restart sshd
-echo **SUCCESS**
+echo "PasswordAuthentication no" | sudo tee -a /etc/ssh/sshd_config
+sudo systemctl restart sshd
+echo "**SSH configuration updated successfully**"
 
-# Configure Apache2 web server
-ufw allow in "Apache Full"
-systemctl enable apache2
-echo **SUCCESS**
+# Configure and enable Apache2 web server
+echo "Configuring Apache2 web server"
+sudo ufw allow in "Apache Full"
+sudo systemctl enable apache2
+echo "**Apache2 web server configured successfully**"
 
-# Configure Squid web proxy
-sudo bash -c "sed -i 's/http_access deny all/http_access allow all/' /etc/squid/squid.conf"
-echo **SUCCESS**
-
-# Enable Squid service
+# Configure Squid web proxy to allow all HTTP access
+echo "Configuring Squid proxy to allow HTTP access for all"
+sudo sed -i 's/http_access deny all/http_access allow all/' /etc/squid/squid.conf
 sudo systemctl enable squid
-echo **SUCCESS**
+echo "**Squid proxy configured successfully**"
 
-# Configure firewall
-ufw allow ssh
-ufw allow https
-ufw allow http
-ufw allow 3128
-ufw enable
-echo **SUCCESS**
+# Configure firewall settings
+echo "Configuring firewall rules"
+sudo ufw allow ssh
+sudo ufw allow https
+sudo ufw allow http
+sudo ufw allow 3128
+echo "y" | sudo ufw enable
+echo "**Firewall configured successfully**"
 
-# Create user accounts and set up ssh keys
-sudo mkdir /home/dennis
-sudo useradd -m -d /home/dennis -s /bin/bash dennis
-sudo mkdir /home/aubrey
-sudo useradd -m -d /home/aubrey -s /bin/bash aubrey
-sudo mkdir /home/captain
-sudo useradd -m -d /home/captain -s /bin/bash captain
-sudo mkdir /home/snibbles
-sudo useradd -m -d /home/snibbles -s /bin/bash snibbles
-sudo mkdir /home/brownie
-sudo useradd -m -d /home/brownie -s /bin/bash brownie
-sudo mkdir /home/scooter
-sudo useradd -m -d /home/scooter -s /bin/bash scooter
-sudo mkdir /home/sandy
-sudo useradd -m -d /home/sandy -s /bin/bash sandy
-sudo mkdir /home/perrier
-sudo useradd -m -d /home/perrier -s /bin/bash perrier
-sudo mkdir /home/cindy
-sudo useradd -m -d /home/cindy -s /bin/bash cindy
-sudo mkdir /home/tiger
-sudo useradd -m -d /home/tiger -s /bin/bash tiger
-sudo mkdir /home/yoda
-sudo useradd -m -d /home/yoda -s /bin/bash yoda
-echo **SUCCESS**
-
-# Set up ssh keys for all users
-for user in dennis aubrey captain snibbles brownie scooter sandy perrier cindy tiger yoda; do
-    sudo mkdir /home/$user/.ssh
-    sudo ssh-keygen -t rsa -f /home/$user/.ssh/id_rsa -N ''
-    sudo ssh-keygen -t ed25519 -f /home/$user/.ssh/id_ed25519 -N ''
-    sudo cat /home/$user/.ssh/*.pub > /home/$user/.ssh/authorized_keys
-    sudo chmod 700 ~/.ssh
-    sudo chmod 600 ~/.ssh/*
-    sudo chown -R $user:$user /home/$user/.ssh
-    sudo chmod 644 "/home/dennis/.ssh/authorized_keys
-
+# Create user accounts
+echo "Creating user accounts"
+for user in "${USERS[@]}"; do
+    sudo useradd -m -s /bin/bash "$user"
+    echo "User account created: $user"
 done
-echo **SUCCESS**
+echo "**User accounts created successfully**"
+
+# Set up SSH keys for each user
+echo "Setting up SSH keys for each user"
+for user in "${USERS[@]}"; do
+    sudo mkdir -p /home/$user/.ssh
+    sudo ssh-keygen -t rsa -f /home/$user/.ssh/id_rsa -N '' -q
+    sudo ssh-keygen -t ed25519 -f /home/$user/.ssh/id_ed25519 -N '' -q
+    sudo cat /home/$user/.ssh/*.pub > /home/$user/.ssh/authorized_keys
+    sudo chmod 700 /home/$user/.ssh
+    sudo chmod 600 /home/$user/.ssh/*
+    sudo chown -R $user:$user /home/$user/.ssh
+    echo "SSH keys configured for user: $user"
+done
+echo "**SSH key setup completed successfully**"
